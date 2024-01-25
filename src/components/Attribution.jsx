259522cef5ea273e366d6fa0629b9b72b5dbfb3a
Arturo1502 <149508454+Arturo1502@users.noreply.github.com>
2024-01-24 20:04:56 -0500

    proyecto completo

---
+++ src/components/Attribution.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Attribution() {
+    return (
+
+        <div className='w-full h-10  text-GrayishBlue mb-[2px] text-xs font-bold flex items-center justify-center'>
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">   Frontend Mentor</a>.
+            Coded by <a href="https://www.linkedin.com/in/arturo-octavio-alvarez-zapata-b152571a3/" target='_blank'> Arturo Alvarez</a>
+        </div>
+
+    )
+}
